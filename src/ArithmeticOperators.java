840082854dc35c78d660c65bf570773e3822a2c6
Tania <you@example.com>
2022-11-15 16:44:14 +0100

    es23 - java

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,39 @@
+/**
+ * autore tania
+ */
+public class ArithmeticOperators {
+
+
+        /**
+         * somma
+         */
+        public int sum(int a, int b) {
+            int risultato = a + b;
+            return risultato;
+        }
+
+        /**
+         * sottrazione
+         */
+        public int sub(int a, int b){
+            int risultato = a - b;
+            return risultato;
+        }
+
+        /**
+         * moltiplicazione
+         */
+        public int mult(int a, int b){
+            int risultato = a * b;
+            return risultato;
+        }
+
+        /**
+         * divisione
+         */
+        public int div(int a, int b){
+            int risultato = a / b;
+            return risultato;
+        }
+    }
+
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * main metodo
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+                ArithmeticOperators num = new ArithmeticOperators();
+
+                System.out.println("Sum: " + num.sum(9, 8));
+                System.out.println("Sub: " + num.sub(80, 3));
+                System.out.println("Mul: " + num.mult(55, 2));
+                System.out.println("Sub operation: " + num.div(6, 2));
+
+            }
+        }
